a49c5da2b539429bf8d182a681ad19b90ff64089
Ripak Raj <ripakraj2005@gmail.com>
2024-05-10 18:05:37 +0530

    changes done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import photo from "../../../public/photo.svg"
 
 const HeroSection = () => {
   return (
@@ -61,11 +62,9 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src={photo}
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[250px] h-[250px] lg:w-[350px] lg:h-[350px]"
             />
           </div>
         </motion.div>

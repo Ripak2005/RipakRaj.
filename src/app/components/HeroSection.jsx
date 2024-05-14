89146fc6cfa2960589d3c37fb6a7de54eb9afb72
Ripak Raj <ripakraj2005@gmail.com>
2024-05-14 23:31:09 +0530

    Changes done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
               Contact Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1vSmJBZmLa3ImE-Zl7k__eJrtF9BHus92/view?usp=sharing"
+              href="https://drive.google.com/file/d/1pp9R90Hdp6jPyN-xKEi6CAQq5nLvhF3n/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
